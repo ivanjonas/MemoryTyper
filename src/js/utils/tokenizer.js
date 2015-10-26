02c657d79c451199b36ef8467bb2c53906bcca3f
Ivan Jonas <ivan@jonas.ninja>
2015-10-26 18:50:05 -0400

    docs(tokenizer): updated jsdoc for parse func

--- src/js/utils/tokenizer.js
+++ src/js/utils/tokenizer.js
@@ -13,7 +13,7 @@ exports.test = function () {
  * Given a String, returns an array of objects. Each object represents metadata about each word in the input. An input
  * with 5 words (as "words" are defined by this tokenizer) would return an array of five elements.
  *
- * Each object contains two distinct elements: the word itself and its starting position in the original input.
+ * Each object contains two distinct elements: the `word` itself and its starting `pos`ition in the original input.
  *
  * @param text
  */
